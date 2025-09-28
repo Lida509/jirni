627f102523fff5c90d943d4006722bbcc305c6c3
Lida509 <144234608+Lida509@users.noreply.github.com>
2025-09-28 21:46:59 +0300

    Update script-ver3.js

--- script-ver3.js
+++ script-ver3.js
@@ -370,5 +370,5 @@ points = STARTING_POINTS;
 setLevel(1);
 updateLevelButtons();
 updatePointsUI();
-setMessage("Готово! Починай кодити — коротка мелодія при старті.", true);
+setMessage("Починай кодити!", true);
 soundEvent('start');
